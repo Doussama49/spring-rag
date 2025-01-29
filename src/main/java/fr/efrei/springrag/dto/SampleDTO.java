e353f84f3b0f49371ad0b520b0f42990e32a57a6
Oussama Daghoui <oussama.daghoui@efrei.net>
2025-01-29 10:13:12 +0100

    #17 add test

---
+++ src/main/java/fr/efrei/springrag/dto/SampleDTO.java
@@ -0,0 +1,19 @@
+package fr.efrei.springrag.dto;
+
+public class SampleDTO {
+
+    private String value;
+
+    public SampleDTO(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
--- src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
@@ -1,4 +1,6 @@
 package fr.efrei.springrag.web.rest;
+import fr.efrei.springrag.dto.SampleDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +11,11 @@ public class SampleResource {
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value") String value) {
         return "Hello " + value + "!";
+    }
+    // Endpoint pour retourner un DTO en JSON
+    @GetMapping("/samples/dto/{value}")
+    public ResponseEntity<SampleDTO> helloDto(@PathVariable(value = "value") String value) {
+        SampleDTO sampleDTO = new SampleDTO("Hello " + value + "!");
+        return ResponseEntity.ok(sampleDTO);
     }
 }
